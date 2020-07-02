ee6d7b84350ab526c02a44e7c85867ebcc55cf1f
Oluwatobii <bellotobi10@gmail.com>
2020-07-02 21:06:54 +0000

    making a test using mocha and chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,29 +1,29 @@
 const request = require("request");
-//console.log(Object.keys(request));
 
-let args = process.argv.slice(2); // is an array [];
+const fetchBreedDescription = function(breedName, callback) {
+  const URL = "https://api.thecatapi.com/v1/breeds/search?q=" + breedName;
+  request.get(URL, (error, response, body) => {
+    console.log("error:", error); // Print the error if one occurred
+    console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
 
-const URL = "https://api.thecatapi.com/v1/breeds/search?q=" + args[0];
+    if (error) {
+      // This means if error !== null (truthy and falsy values)
+      return callback(error, null);
+      //process.exit();
+    }
 
-request.get(URL, (error, response, body) => {
-  console.log("error:", error); // Print the error if one occurred
-  console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+    const breedsArr = JSON.parse(body);
 
-  if (error) {
-    // This means if error !== null (truthy and falsy values)
-    console.log("The website is broken");
-    process.exit();
-  }
+    if (breedsArr.length !== 0) {
+      let breedObj = breedsArr[0];
+      let description = breedObj.description;
+      callback(null, description);
+      //process.exit();
+    } else {
+      callback("This file cannot be found");
+      //process.exit();
+    }
+  });
+};
 
-  const breedsArr = JSON.parse(body);
-
-  if (breedsArr.length !== 0) {
-    let breedObj = breedsArr[0];
-    let des = breedObj.description;
-    console.log(des);
-    process.exit();
-  } else {
-    console.log("This file cannot be found");
-    process.exit();
-  }
-});
+module.exports = { fetchBreedDescription };
